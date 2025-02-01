899f8a37cd55e290f26502559660ecd0b3bc9c54
Sam Hudson <samhudson@outlook.com>
2025-02-01 01:01:00 +0000

    updated math_data
    - meanings of indexes are now taken from meta.json file
    - added optional argument to only get specific folders

--- src/utils/math_data.py
+++ src/utils/math_data.py
@@ -3,34 +3,44 @@ from PIL import Image
 import numpy as np
 import os
 import pandas as pd
+import json
 import matplotlib
 from utils.utils import one_hot
 matplotlib.use('module://matplotlib-backend-kitty')
 
 
-def load_math_data(path: str):
+def load_math_data(path: str, folders: list[str] | None = None):
     # check for folders in path
-    folders = os.listdir(path)
-    meanings = {}
+    if folders is None:
+        folders = os.listdir(path)
+    meanings = load_math_meta()
     images = []
     labels = []
-    for i, folder in enumerate(folders):
+    for folder in folders:
         print(f'opening folder {folder}')
-        meanings[i] = folder
+        i = list(meanings.values()).index(folder)
+        print(i)
         # check for images in folder
         files = os.listdir(os.path.join(path, folder))
         for image in files:
             img = Image.open(os.path.join(path, folder, image))
+            img = img.convert('L')
             img = img.resize((28, 28))
             arr = np.array(img)
             images.append(arr)
             labels.append(i)
+
     x, y = process(images, labels)
     x_train = x[:-6000]
     y_train = y[:-6000]
     x_test = x[-6000:]
     y_test = y[-6000:]
     return x_test, y_test, x_train, y_train, meanings
+
+
+def load_math_meta() -> dict:
+    with open('data/math/meta.json', 'r') as f:
+        return json.load(f)
 
 
 def process(images, labels):
